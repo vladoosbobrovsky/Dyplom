ffa6a05076b9f777cb288b5411c18e35af69e191
Vladyslav Bobrovskyi <vladyslav.bobrovskyi@globallogic.com>
2021-03-19 17:44:36 +0200

    upd

--- devops.sh
+++ devops.sh
@@ -2,11 +2,13 @@
 
 PATH_TO_DIR=spring-petclinic/
 BOXES=$"boxes -d stone -p a2v1"
+DOCKER_TAG=devops-spring-petclinic
+DOCKER_MAINTAINER=20000903
 
 if [ -d $PATH_TO_DIR ] 
 then
     echo "Directory $PATH_TO_DIR exists... Deleting" | $BOXES
-    rm -rf spring-petclinic/
+    rm -rf $PATH_TO_DIR
 else
     echo "Error: Directory $PATH_TO_DIR does not exists." | $BOXES
 fi
@@ -23,8 +25,14 @@ cd spring-petclinic
 # Docker image build
 echo "Docker image build" | $BOXES
 cd ..
-docker build -t 20000903/spring-petclinic:devops-spring-petclinic .
+docker build -t $DOCKER_MAINTAINER/spring-petclinic:$DOCKER_TAG .
 
 #Docker image build result 
-echo "Docker images result" | $BOXES
-docker images | grep devops-spring-petclinic
+echo "Results for tag - $DOCKER_TAG is below" | $BOXES
+docker images | grep $DOCKER_TAG
+
+echo "Push Docker image to the DockerHub" | $BOXES
+
+docker push $DOCKER_MAINTAINER/spring-petclinic:$DOCKER_TAG
+
+echo "Docker image is successfuly pushed to Hub !!!" | $BOXES
