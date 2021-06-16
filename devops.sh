60130f8ff751ad4b96693bf1fad190304d8a880a
vladoosbobrovsky <500kg.ggwp@gmail.com>
2021-06-16 13:27:37 +0300

    script upd

--- devops.sh
+++ devops.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 PATH_TO_DIR=spring-petclinic/
 BOXES=$"boxes -d stone -p a2v1"
 DOCKER_TAG=devops-spring-petclinic
@@ -12,15 +11,12 @@ then
 else
     echo "Error: Directory $PATH_TO_DIR does not exists." | $BOXES
 fi
-
-
-
 # Download the Spring-Petclinic and build it
 echo "Download the Spring-Petclinic and build it" | $BOXES
 git clone https://github.com/spring-projects/spring-petclinic.git
 cd spring-petclinic
+sed 's/h2/mysql/'  src/main/resources/application.properties
 ./mvnw package -DskipTests
-
 
 # Docker image build
 echo "Docker image build" | $BOXES
@@ -32,7 +28,5 @@ echo "Results for tag - $DOCKER_TAG is below" | $BOXES
 docker images | grep $DOCKER_TAG
 
 echo "Push Docker image to the DockerHub" | $BOXES
-
 docker push $DOCKER_MAINTAINER/spring-petclinic:$DOCKER_TAG
-
 echo "Docker image is successfuly pushed to Hub !!!" | $BOXES
--- dockerfile
+++ dockerfile
@@ -1,5 +1,5 @@
 FROM openjdk:8-jdk
-MAINTAINER 500kg.ggwp@gmail.com
+LABEL maintainer="500kg.ggwp@gmail.com" 
 
 RUN useradd -u 1000 vladyslav
 USER vladyslav
